6a526cb914b6c4693aeadf0ed428a1bc063dd919
Mythamor <bethmithamo.bm@gmail.com>
2023-09-11 16:04:33 +0300

    My Square fix

--- square.py
+++ square.py
@@ -1,18 +1,36 @@
 #!/usr/bin/python3
 
-class square():
+class Square():
     
-    width = 0
-    height = 0
+    def __init__(self, width=0, height=0):
+        self._width = width
+        self._height = height
 
-    
-    def __init__(self, *args, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+    @property
+    def width(self):
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        if value > 0:
+            self._width = value
+        else:
+            raise ValueError("Width must be a positive")
+
+    @property
+    def height(self):
+        return self._height
+
+    @height.setter
+    def height(self, value):
+        if value > 0:
+            self._height = value
+        else:
+            raise ValueError("Height must be positive")
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
     def PermiterOfMySquare(self):
         return (self.width * 2) + (self.height * 2)
@@ -22,7 +40,7 @@ class square():
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
