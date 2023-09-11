a4c858d53015359d93786d3c093bf8a117538560
Mythamor <bethmithamo.bm@gmail.com>
2023-09-11 16:49:32 +0300

    My Square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -16,46 +16,29 @@ class Square():
         __str__(self)
     """
 
-    def __init__(self, width=0, height=0):
+    width = 0
+    height = 0
+
+    def __init__(self, *args, **kwargs):
         """Initialize square instance with width and height"""
-        self._width = width
-        self._height = height
-
-    @property
-    def width(self):
-        """ Width getter attribute """
-        return self._width
-
-    @width.setter
-    def width(self, value):
-        """ Width setter attribute """
-        if value > 0:
-            self._width = value
-        else:
-            raise ValueError("Width must be a positive")
-
-    @property
-    def height(self):
-        """ Height getter attribute """
-        return self._height
-
-    @height.setter
-    def height(self, value):
-        """ Height setter atttribute """
-        if value > 0:
-            self._height = value
-        else:
-            raise ValueError("Height must be positive")
+        if kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+        if 'width' in kwargs.keys() and self.height != self.width:
+            self.height = self.width
+        if 'height' in kwargs.keys() and self.width != self.height:
+            self.width = self.height
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self._width * self._height
+        return self.width * self.height
 
     def PermiterOfMySquare(self):
-        return (self._width * 2) + (self._height * 2)
+        """ Perimeter of the square """
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        return "{}/{}".format(self._width, self._height)
+        return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
