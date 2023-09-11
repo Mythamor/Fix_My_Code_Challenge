9d7993f91ae49b5ca17ab7c2b60fa65c6c1bfae2
Mythamor <bethmithamo.bm@gmail.com>
2023-09-11 17:01:44 +0300

    Fixed OOP

--- 0x01-challenge/user.py
+++ 0x01-challenge/user.py
@@ -1,28 +1,35 @@
 #!/usr/bin/python3
 """ 
-User class
+User class module
 """
 
 class User():
-    """ Documentation """
+    """ Defines a class user
+        Attr:
+            email
+        Methods:
+            email getter
+            email setter
+    """
 
     def __init__(self):
-        """ Documentation """
+        """ Initialize a User instance """
         self.__email = None
+    
+    @property
+    def email(self):
+        """ Define the email getter """
+        return self.__email
 
     @email.setter
     def email(self, value):
-        """ Documentation """
-        if type(value) is not str:
+        """ Define the email setter """
+        if isinstance(value, str):
+            self.__email = value
+        else:
             raise TypeError("email must be a string")
-        self.__email = value
 
-    @property
-    def email(self):
-        """ Documentation """
-        return self.__email
-   
-    
+
 if __name__ == "__main__":
 
     u = User()
