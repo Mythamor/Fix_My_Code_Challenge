1c7482eeb30adcb68f276b78963f05c8b4961a73
Mythamor <bethmithamo.bm@gmail.com>
2023-09-11 16:38:34 +0300

    My Square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -10,22 +10,25 @@ class Square():
         height
     Methods:
         __init__
-        area(self)
-        perimeter(self)
+        area_of_my_square(self)
+        PerimeterOfMySquare(self)
         __str__(self)
     """
 
 
     def __init__(self, width=0, height=0):
+        """Initialize square instance with width and height"""
         self._width = width
         self._height = height
 
     @property
     def width(self):
+        """ Width getter attribute """
         return self._width
 
     @width.setter
     def width(self, value):
+        """ Width setter attribute """
         if value > 0:
             self._width = value
         else:
@@ -33,29 +36,31 @@ class Square():
 
     @property
     def height(self):
+        """ Height getter attribute """
         return self._height
 
     @height.setter
     def height(self, value):
+        """ Height setter atttribute """
         if value > 0:
             self._height = value
         else:
             raise ValueError("Height must be positive")
 
-    def area(self):
+    def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.height
+        return self._width * self._height
 
-    def permiter(self):
-        return (self.width * 2) + (self.height * 2)
+    def PermiterOfMySquare(self):
+        return (self._width * 2) + (self._height * 2)
 
     def __str__(self):
-        return "{}/{}".format(self.width, self.height)
+        return "{}/{}".format(self._width, self._height)
 
 
 if __name__ == "__main__":
 
     s = Square(width=12, height=9)
     print(s)
-    print(s.area())
-    print(s.permiter())
+    print(s.area_of_my_square())
+    print(s.PermiterOfMySquare())
